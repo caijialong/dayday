2f14c60305910904611b41f7e6172103059b8d42
caijialong <568923969@qq.com>
2017-03-31 15:35:04 +0800

    本次提交

--- dailyfresh/dailyfresh/urls.py
+++ dailyfresh/dailyfresh/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^xiaocai/', include(xiaocai.urls)),
+    url(r'^xiaocai/', include(cai.urls)),
 ]
